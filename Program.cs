df9eb98ebcc785a55b31c544099fe8c6dc64394f
MattFofo <matteo.segattini2@gmail.com>
2022-06-27 14:42:35 +0200

    snack 3 e 4

--- Program.cs
+++ Program.cs
@@ -2,6 +2,46 @@
 {
     static void Main()
     {
+        // Snack 3) - Calcola la somma e la media dei numeri da 2 a 10
+
+        int somma = 0;
+
+        for (int i = 2; i < 10; i++)
+        {
+            somma += i;
+        }
+
+        float media = somma / 8;
+
+        Console.WriteLine(somma);
+        Console.WriteLine(media);
+
+
+
+
+        return;
+        
+         //Snack 3) -Il software deve chiedere per 10 volte all’utente di inserire un numero. Il programma stampa la somma di tutti i numeri inseriti
+         
+        
+
+        int sum = 0;
+        for (int i = 0; i < 10; i++)
+        {
+            
+            if (i == 0)
+            {
+                Console.WriteLine("Scrivi un numero");
+            } else
+            {
+                Console.WriteLine("scrivi un'altro numero");
+            }
+
+            int userNumber = int.Parse(Console.ReadLine());
+            sum += userNumber;
+        }
+
+        Console.WriteLine("La somma dei numeri è: " + sum);
 
     }
 }
