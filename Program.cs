3bf276fc8d865fc47f87a00174756b5d56ac43b4
MattFofo <matteo.segattini2@gmail.com>
2022-06-27 15:42:13 +0200

    snack 5

--- Program.cs
+++ Program.cs
@@ -2,6 +2,40 @@
 {
     static void Main()
     {
+        // Snack 5) - Chiedi all'utente un numero N. Crea N array, ognuno formato da 10 numeri casuali tra 1 e 100, e ogni volta che ne crei uno stampalo a schermo
+
+        Console.WriteLine("Scrivi un numero");
+        int anotherGuyNumber = int.Parse(Console.ReadLine());
+        Random rnd = new Random();
+        int[] arrayNumbers = new int[10];
+
+        for (int i = 0; i < anotherGuyNumber; i++)
+        {
+            
+            for (int j = 0; j < arrayNumbers.Length; j++)
+            {
+                arrayNumbers[j] = rnd.Next(1, 101);
+            }
+
+            for (int k = 0; k < arrayNumbers.Length; k++)
+            {
+                if (k == arrayNumbers.Length - 1)
+                {
+                    Console.Write(arrayNumbers[k]);
+                } else
+                {
+                    Console.Write(arrayNumbers[k] + "-");
+                }
+                
+            }
+
+            Console.WriteLine();
+        }
+
+
+        return;
+
+
         // Snack 5) - Chiedi all'utente un numero. Se è pari stampalo, se è dispari stampa il successivo
 
         Console.WriteLine("Scrivi un numero");
@@ -16,10 +50,9 @@
 
         }
 
-
-
-
         return;
+
+
 
         // Snack 3) - Calcola la somma e la media dei numeri da 2 a 10
 
@@ -36,14 +69,12 @@
         Console.WriteLine(media);
 
 
-
-
         return;
         
-         //Snack 3) -Il software deve chiedere per 10 volte all’utente di inserire un numero. Il programma stampa la somma di tutti i numeri inseriti
-         
-        
 
+
+        //Snack 3) -Il software deve chiedere per 10 volte all’utente di inserire un numero. Il programma stampa la somma di tutti i numeri inseriti
+         
         int sum = 0;
         for (int i = 0; i < 10; i++)
         {
